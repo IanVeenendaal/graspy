39c2fb4590bdb815ec89739edb7d295f863a89cb
Ian Veenendaal <veenendaali@cardiff.ac.uk>
2025-12-15 16:17:33 +0000

    test: write edi parsing tests

--- tests/test_edi_parse.py
+++ tests/test_edi_parse.py
@@ -1,41 +1,13 @@
-import xml.etree.ElementTree as ET
 from pathlib import Path
+from graspy.edi import parse_edi_beamwidths, parse_edi_crosspol
 
 dir = Path(__file__).parent.parent / "tests"
 file = "test.edi"
 filename = dir / file
 
-tree = ET.parse(filename)
-root = tree.getroot()
 
-ns = {"edi": "http://www.edi-forum.org"}
+bw_df = parse_edi_beamwidths(filename)
+print(bw_df)
 
-
-# Get Frequencies
-freqs = (
-    root.find(".//edi:Variable[@Class='Frequency']", ns)
-    .find("edi:Component", ns)
-    .find("edi:Value", ns)
-    .text.split()
-)
-freqs = [float(freq) for freq in freqs]
-print(f"Frequencies: {freqs}")
-
-# Get Beamwidth Data
-levels = (
-    root.find(".//edi:Variable[@Class='BeamWidthLevels']", ns)
-    .find("edi:Component", ns)
-    .find("edi:Value", ns)
-    .text.split()
-)
-levels = [float(lvl) for lvl in levels]
-print(f"Beamwidth Levels: {levels}")
-
-
-data = root.find("edi:Data", ns)
-bw = data.find(".//edi:Variable[@Name='smooth_radiating_device_BeamWidth']", ns)
-comp = bw.find("edi:Component", ns)
-bw_value = comp.find("edi:Value", ns)
-bw_values = bw_value.text.split()
-bw_values = [float(val) for val in bw_values]
-print(f"Beamwidth Values: {bw_values}")
+crosspol_df = parse_edi_crosspol(filename)
+print(crosspol_df)
